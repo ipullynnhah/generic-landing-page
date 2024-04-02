96453eb1ff70a4f65a3fda46b5b40a82e4269a6e
ipullynnhah <ipullynnhah@gmail.com>
2024-04-01 21:22:58 -0300

    Tarefa 5 concluída

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,7 +2,6 @@ const modal = document.querySelector(".modal");
 const headerBtn = document.querySelector(".header__btn");
 const faqBtn = document.querySelector(".faq__btn");
 const modalCloseBtn = document.querySelector(".modal__btn--close");
-const modalForm = document.querySelector(".modal__form");
 
 headerBtn.addEventListener("click", () => modal.showModal());
 faqBtn.addEventListener("click", () => modal.showModal());
